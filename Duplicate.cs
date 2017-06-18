517eb299102172bdb3daa6b79dc5c453e4ee595c
Ernesto <mr.ernestocampese@gmail.com>
2017-06-18 16:41:26 +0100

    Check if there are duplicate char on a String!

--- Duplicate.cs
+++ Duplicate.cs
@@ -11,10 +11,6 @@ namespace Duplicate
         static void Main(string[] args)
         {
 
-            //Write a program and ask the user to enter a few numbers separated by a hyphen. If the user simply presses Enter, 
-            //without supplying an input, exit immediately;
-            //otherwise, check to see if there are duplicates. If so, display "Duplicate" on the console.
-
             Console.Write("Please enter few numbers separated by a hyphen (-): ");
             var userInput = Console.ReadLine();
 
